60dce4701d7e12d4b7f66048c68a89dae08d37f1
simarc2907 <simarc2907@gmail.com>
2024-03-10 11:58:17 +0530

    about page added

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get('/home')#if somebody goes to this url then the below fn is executed
 def home():
     return "Hello World!"
+
+@app.get('/about')
+def about():
+    return "This is the about page"
